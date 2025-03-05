cbc586e30712e9b21d05fb2ebf1e03838c3276fa
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-03-05 07:47:33 -0500

    Adding and styling Contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import '../styles/Contact.scss';
-import IMAGE from '../assets/menu/pizzaLeft.jpg'
+import "../styles/Contact.scss";
+import IMAGE from "../assets/menu/pizzaLeft.jpg";
 
 export default function Contact() {
   return (
@@ -8,7 +8,7 @@ export default function Contact() {
       <figure className="figure">
         <img src={IMAGE} alt="" className="image__contact" />
       </figure>
-      <form action="" className="contact__form">
+      <form action="submit" className="form__contact">
         <fieldset className="form__fieldset" aria-label="fieldset">
           <legend className="form__legend" aria-label="fieldset">
             Contact Us
@@ -19,29 +19,33 @@ export default function Contact() {
           <input
             id="full__name"
             type="text"
-            className="form__input"
+            className="form__input placeHolder"
             placeholder="Your Full Name"
           />
-          <label htmlFor="email" className="form__email">
+
+          <label htmlFor="email" className="form__label">
             Email Address
           </label>
           <input
             id="email"
             type="email"
-            className="form__input"
+            className="form__input placeHolder"
             placeholder="Your Email Address"
           />
-          <label htmlFor="form__message" className="form__textarea">
+          <label htmlFor="form__message" className="form__label">
             Message
           </label>
           <textarea
             name="message"
             id="form__message"
-            className="form__textarea"
+            className="form__textarea placeHolder"
             placeholder="Your Message"
+            rows={5}
+            cols={6}
+            
           ></textarea>
         </fieldset>
-        <button type="submit" id="form__submit">
+        <button type="submit" id="form__submit" className="form__submit">
           Submit
         </button>
       </form>
